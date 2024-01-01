aad538e2f4d96939b66889a26ec07166a5888bbd
Junpei Tsuji <junpei.tsuji.ams@gmail.com>
2024-01-01 18:19:39 +0900

    feat: added github org authorization

--- app/packages/backend/src/plugins/catalog.ts
+++ app/packages/backend/src/plugins/catalog.ts
@@ -1,4 +1,5 @@
 import { CatalogBuilder } from '@backstage/plugin-catalog-backend';
+import { GithubOrgEntityProvider } from '@backstage/plugin-catalog-backend-module-github';
 import { ScaffolderEntitiesProcessor } from '@backstage/plugin-catalog-backend-module-scaffolder-entity-model';
 import { Router } from 'express';
 import { PluginEnvironment } from '../types';
@@ -8,6 +9,19 @@ export default async function createPlugin(
 ): Promise<Router> {
   const builder = await CatalogBuilder.create(env);
   builder.addProcessor(new ScaffolderEntitiesProcessor());
+  // The org URL below needs to match a configured integrations.github entry
+  // specified in your app-config.
+  builder.addEntityProvider(
+    GithubOrgEntityProvider.fromConfig(env.config, {
+      id: 'production',
+      orgUrl: 'https://github.com/jun06t-org',
+      logger: env.logger,
+      schedule: env.scheduler.createScheduledTaskRunner({
+        frequency: { minutes: 60 },
+        timeout: { minutes: 15 },
+      }),
+    }),
+  );
   const { processingEngine, router } = await builder.build();
   await processingEngine.start();
   return router;
